b340d229e890da91019d77400938eb2e65a62030
MichaelGardone <michaelgardone@gmail.com>
2024-08-17 16:37:15 -0600

    changing ui, hang on

--- scripts/game/game_overlay.gd
+++ scripts/game/game_overlay.gd
@@ -38,8 +38,9 @@ func show_brief():
 	$CaseBrief/Casebrief_Vbox/Header_Hbox/Case_Number_Letter.text = display_text
 	$CaseBrief/Casebrief_Vbox/Header_Hbox/CaseName_Header.text = _case.CASE_NAME
 	
-	# Split into lines based on new lines
-	var lines = _case.CASE_DESCRIPTION.split("\n")
+	$CaseBrief/Casebrief_Vbox/Casebrief_Body.visible = false
+	
+	var lines = _case.CASE_DESCRIPTION
 	
 ##
 
