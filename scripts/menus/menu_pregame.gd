52f89882a371818c736cc5868190bcd818e49f41
SBrianZ <brian@brianzavala.com>
2024-08-19 22:12:36 -0700

    Pregame shit

--- scripts/menus/menu_pregame.gd
+++ scripts/menus/menu_pregame.gd
@@ -3,8 +3,25 @@ extends Control
 signal pregame_to_game
 signal pregame_to_main
 
+@onready var pregame_sheet = $Pregame
+
 func _on_tutorial_button_pressed():
 	pregame_to_game.emit()
 
 func _on_back_button_pressed():
 	pregame_to_main.emit()
+
+func _on_decrease_button_pressed():
+	if pregame_sheet.frame != 0:
+		pregame_sheet.frame -=1
+	elif pregame_sheet.frame == 0:
+		pregame_sheet.frame = 6
+
+func _on_increase_button_pressed():
+	if pregame_sheet.frame != 6:
+		pregame_sheet.frame +=1
+	elif pregame_sheet.frame == 6:
+		pregame_sheet.frame = 0
+
+func _on_skip_button_pressed():
+	pregame_sheet.frame = 6
